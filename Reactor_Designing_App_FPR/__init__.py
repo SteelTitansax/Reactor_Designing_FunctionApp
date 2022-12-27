db81ff16a6d4a994df190e25fda16ba6cfa39f53
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2022-12-28 00:09:24 +0100

    Update __init__.py

--- Reactor_Designing_App_FPR/__init__.py
+++ Reactor_Designing_App_FPR/__init__.py
@@ -2,37 +2,10 @@
 import logging
 from scipy.integrate import quad
 import azure.functions as func
-
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Reactor designing Begins...')
-    
-    
-    """
-    server = 'SERVERNAME\INSTANCEID,64346'
-    database = 'GRAPHAPI' 
-    username = 'uname' 
-    password = 'Happy2020'
-
-    def insert_reactor_db(reactor_df,server,database,username,password):
-    #Create a connection string
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor()
- 
-    for index in range(reactor_df.shape[0]): 
-        #insert into table 
-        try:
-            insert_query = “INSERT INTO GRAPHAPI.dbo.[CSTR_Reactor] ([Ca0],[k],[Volume],[rA],[X]) VALUES (?,?,?,?,?)”
-            cursor.execute(insert_query,reactor_df[‘Ca0’][index],reactor_df[‘k’][index],reactor_df[‘Volume’][index],reactor_df[‘rA’][index],reactor_df[‘X’][index])
-        except:
-            cnxn.rollback()
-        finally:
-            cnxn.commit()
-            cnxn.close()
-            #Call the function
-            insert_reactor_db(users_df,server,database,username,password)
-    """
-
 
     try:
         req_body = req.get_json()
@@ -64,13 +37,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         V,err = quad(integral,0,X)
         logging.info("Volumen : " + str(V) )
-        # reactor_dict = {‘Ca0’:Ca0,'k':k,'Volume':V,'rA':rA,'X':X}
-        # reactor_df = pd.DataFrame(data=reactor_dict)
-        # insert_reactor_db(reactor_df,server,database,username,password)
-    
+
+        return json.dumps(
+        { 
+            "Volume": V,
+            "Ca0": Ca0,
+            "rA": rA(X),
+            "k" : k,
+            "X" : X
+        }
+        )
     return func.HttpResponse("Reactor designed successfully")
    
-
 
 
 
